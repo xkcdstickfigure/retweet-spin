4d1701a1fd63c4172972796af3f7533bd2f7515e
Archie Baer <archie@abaer.dev>
2020-11-30 17:48:53 +0000

    Moved retweeter code to file

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 require("dotenv").config();
-const {
-    TWEET,
-    CONSUMER_KEY,
-    CONSUMER_SECRET,
-    ACCESS_TOKEN,
-    ACCESS_TOKEN_SECRET
-} = process.env;
-
-const Twit = require("twit");
-const t = new Twit({
-    consumer_key: CONSUMER_KEY,
-    consumer_secret: CONSUMER_SECRET,
-    access_token: ACCESS_TOKEN,
-    access_token_secret: ACCESS_TOKEN_SECRET
-});
-
-const getRetweeter = async () => (
-    await t.get("users/show", {
-        user_id: (
-            await t.get("statuses/retweeters/ids", {
-                id: TWEET,
-                count: 1
-            })
-        ).data.ids[0]
-    })
-).data;
+const getRetweeter = require("./retweeter");
 
 getRetweeter().then(console.log);
---
+++ retweeter.js
@@ -0,0 +1,26 @@
+const {
+    TWEET,
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET
+} = process.env;
+
+const Twit = require("twit");
+const t = new Twit({
+    consumer_key: CONSUMER_KEY,
+    consumer_secret: CONSUMER_SECRET,
+    access_token: ACCESS_TOKEN,
+    access_token_secret: ACCESS_TOKEN_SECRET
+});
+
+module.exports = async () => (
+    await t.get("users/show", {
+        user_id: (
+            await t.get("statuses/retweeters/ids", {
+                id: TWEET,
+                count: 1
+            })
+        ).data.ids[0]
+    })
+).data;
